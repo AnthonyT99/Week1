be498428fab61606f662dab33e54dc86fb3ec690
anthony t <anthonysun1@hotmail.com>
2021-03-20 01:09:43 -0400

    added js

---
+++ index.js
@@ -0,0 +1,11 @@
+let itemPrice = 19.99;
+let wallet = 50.00;
+let numberOfFriends = 5;
+let age = 19;
+let firstName = "Anthony";
+let lastName = " Terrio";
+let middleInitial = " R";
+
+console.log('Balance left: $' + (wallet - itemPrice));
+console.log('Friends gained: ' + age * numberOfFriends);
+console.log('Full Name: ' + firstName + middleInitial + lastName);
